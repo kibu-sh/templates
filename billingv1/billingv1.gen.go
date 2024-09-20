363fb0b22f8a1c43277000a468f1c51b2295b412
James R Qualls <Qualls.James@gmail.com>
2024-09-20 12:19:39 -0700

    code gen sample

--- billingv1/billingv1.gen.go
+++ billingv1/billingv1.gen.go
@@ -3,11 +3,11 @@ package billingv1
 import (
 	"context"
 	"github.com/kibu-sh/kibu/pkg/transport/temporal"
+	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/workflow"
 )
 
-////go:generate go run github.com/kibu-sh/kibu/cmd/kibu-gen@latest
-
+// Constants for workflow and activity names.
 const (
 	barv1ServiceWatchBillingBillingName               = "barv1.WatchBillingBilling"
 	barv1CustomerBillingWorkflowName                  = "barv1.customerBillingWorkflow"
@@ -18,10 +18,17 @@ const (
 	barv1ActivitiesChargePaymentMethodName            = "barv1.activities.ChargePaymentMethod"
 )
 
+type WorkflowOptionFunc = temporal.WorkflowOptionFunc
+type ActivityOptionFunc = temporal.ActivityOptionFunc
+type UpdateOptionFunc = temporal.UpdateOptionFunc
+
+// GetHandleOpts for getting workflow handles.
 type GetHandleOpts struct {
 	WorkflowID string
 	RunID      string
 }
+
+// Interfaces and structs implementing them.
 
 type CustomerBillingWorkflowRun interface {
 	ID() string
@@ -29,23 +36,22 @@ type CustomerBillingWorkflowRun interface {
 
 	Get(ctx context.Context) (CustomerBillingResponse, error)
 
-	AttemptPayment(ctx context.Context, req AttemptPaymentRequest, mods ...temporal.WorkflowOptionFunc) (res AttemptPaymentResponse, err error)
+	AttemptPayment(ctx context.Context, req AttemptPaymentRequest, mods ...WorkflowOptionFunc) (AttemptPaymentResponse, error)
 
-	GetAccountDetails(ctx context.Context, req GetAccountDetailsRequest, mods ...temporal.WorkflowOptionFunc) (res GetAccountDetailsResult, err error)
+	GetAccountDetails(ctx context.Context, req GetAccountDetailsRequest, mods ...WorkflowOptionFunc) (GetAccountDetailsResult, error)
 
-	CancelBilling(ctx context.Context, req CancelBillingSignal, mods ...temporal.WorkflowOptionFunc) error
-	SetDiscount(ctx context.Context, req SetDiscountSignal, mods ...temporal.WorkflowOptionFunc) error
+	CancelBilling(ctx context.Context, req CancelBillingSignal, mods ...WorkflowOptionFunc) error
+	SetDiscount(ctx context.Context, req SetDiscountSignal, mods ...WorkflowOptionFunc) error
 }
 
 type CustomerBillingWorkflowClient interface {
-	Execute(ctx context.Context, req CustomerBillingRequest) (run CustomerBillingWorkflowRun, err error)
-	GetHandle(ctx context.Context, ref GetHandleOpts) (run CustomerBillingWorkflowRun, err error)
-	SignalWithStartSetDiscount(ctx context.Context, req SetDiscountSignal) (run CustomerBillingWorkflowRun, err error)
+	Execute(ctx context.Context, req CustomerBillingRequest) (CustomerBillingWorkflowRun, error)
+	GetHandle(ctx context.Context, ref GetHandleOpts) (CustomerBillingWorkflowRun, error)
+	SignalWithStartSetDiscount(ctx context.Context, req SetDiscountSignal) (CustomerBillingWorkflowRun, error)
 }
 
 type CustomerBillingWorkflowChildRun interface {
 	ID() string
-	RunID() string
 	IsReady() bool
 	Underlying() workflow.ChildWorkflowFuture
 	Get(ctx workflow.Context) (CustomerBillingResponse, error)
@@ -67,7 +73,7 @@ type CustomerBillingExternalRun interface {
 }
 
 type CustomerBillingWorkflowChildClient interface {
-	Execute(ctx workflow.Context, req CustomerBillingRequest) (res CustomerBillingResponse, err error)
+	Execute(ctx workflow.Context, req CustomerBillingRequest) (CustomerBillingResponse, error)
 	ExecuteAsync(ctx workflow.Context, req CustomerBillingRequest) CustomerBillingWorkflowChildRun
 	External(ref GetHandleOpts) CustomerBillingExternalRun
 }
@@ -80,169 +86,218 @@ type WorkflowsClient interface {
 	CustomerBilling() CustomerBillingWorkflowClient
 }
 
-var _ WorkflowsClient = (*workflowsClient)(nil)
-var _ WorkflowsProxy = (*workflowsProxy)(nil)
-var _ CustomerBillingWorkflowClient = (*customerBillingWorkflowClient)(nil)
-var _ CustomerBillingWorkflowChildClient = (*customerBillingWorkflowChildClient)(nil)
-var _ CustomerBillingExternalRun = (*customerBillingExternalRun)(nil)
-var _ CustomerBillingWorkflowRun = (*customerBillingWorkflowRun)(nil)
-var _ CustomerBillingWorkflowChildRun = (*customerBillingWorkflowChildRun)(nil)
-
-type workflowsClient struct{}
-
-func (w workflowsClient) CustomerBilling() CustomerBillingWorkflowClient {
-	//TODO implement me
-	panic("implement me")
+type workflowsClient struct {
+	client client.Client
 }
 
+func (w *workflowsClient) CustomerBilling() CustomerBillingWorkflowClient {
+	return &customerBillingWorkflowClient{
+		client: w.client,
+	}
+}
+
+// Implementing WorkflowsProxy
 type workflowsProxy struct{}
 
-func (w workflowsProxy) CustomerBilling() CustomerBillingWorkflowChildClient {
-	//TODO implement me
-	panic("implement me")
+func (w *workflowsProxy) CustomerBilling() CustomerBillingWorkflowChildClient {
+	return &customerBillingWorkflowChildClient{}
 }
 
-type customerBillingWorkflowClient struct{}
-
-func (c customerBillingWorkflowClient) Execute(ctx context.Context, req CustomerBillingRequest) (run CustomerBillingWorkflowRun, err error) {
-	//TODO implement me
-	panic("implement me")
+// Implementing CustomerBillingWorkflowClient
+type customerBillingWorkflowClient struct {
+	client client.Client
 }
 
-func (c customerBillingWorkflowClient) GetHandle(ctx context.Context, ref GetHandleOpts) (run CustomerBillingWorkflowRun, err error) {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowClient) Execute(ctx context.Context, req CustomerBillingRequest) (CustomerBillingWorkflowRun, error) {
+	options := temporal.NewWorkflowOptionsBuilder().
+		WithTaskQueue("default").
+		WithProvidersWhenSupported().
+		AsStartOptions()
+
+	we, err := c.client.ExecuteWorkflow(ctx, options, barv1CustomerBillingWorkflowName, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &customerBillingWorkflowRun{
+		client:      c.client,
+		workflowRun: we,
+	}, nil
 }
 
-func (c customerBillingWorkflowClient) SignalWithStartSetDiscount(ctx context.Context, req SetDiscountSignal) (run CustomerBillingWorkflowRun, err error) {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowClient) GetHandle(ctx context.Context, ref GetHandleOpts) (CustomerBillingWorkflowRun, error) {
+	we := c.client.GetWorkflow(ctx, ref.WorkflowID, ref.RunID)
+	return &customerBillingWorkflowRun{
+		client:      c.client,
+		workflowRun: we,
+	}, nil
 }
 
+func (c *customerBillingWorkflowClient) SignalWithStartSetDiscount(ctx context.Context, req SetDiscountSignal) (CustomerBillingWorkflowRun, error) {
+	options := temporal.NewWorkflowOptionsBuilder().
+		WithTaskQueue("default").
+		WithProvidersWhenSupported().
+		AsStartOptions()
+
+	we, err := c.client.SignalWithStartWorkflow(ctx, options.ID, barv1CustomerBillingWorkflowSetDiscountName, req, options, barv1CustomerBillingWorkflowName, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &customerBillingWorkflowRun{
+		client:      c.client,
+		workflowRun: we,
+	}, nil
+}
+
+// Implementing CustomerBillingWorkflowChildClient
 type customerBillingWorkflowChildClient struct{}
 
-func (c customerBillingWorkflowChildClient) Execute(ctx workflow.Context, req CustomerBillingRequest) (res CustomerBillingResponse, err error) {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowChildClient) Execute(ctx workflow.Context, req CustomerBillingRequest) (CustomerBillingResponse, error) {
+	future := c.ExecuteAsync(ctx, req)
+	return future.Get(ctx)
 }
 
-func (c customerBillingWorkflowChildClient) ExecuteAsync(ctx workflow.Context, req CustomerBillingRequest) CustomerBillingWorkflowChildRun {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowChildClient) ExecuteAsync(ctx workflow.Context, req CustomerBillingRequest) CustomerBillingWorkflowChildRun {
+	options := temporal.NewWorkflowOptionsBuilder().
+		WithTaskQueue("default").
+		WithProvidersWhenSupported().
+		AsChildOptions()
+
+	ctx = workflow.WithChildOptions(ctx, options)
+	childFuture := workflow.ExecuteChildWorkflow(ctx, barv1CustomerBillingWorkflowName, req)
+
+	return &customerBillingWorkflowChildRun{
+		childFuture: childFuture,
+	}
 }
 
-func (c customerBillingWorkflowChildClient) External(ref GetHandleOpts) CustomerBillingExternalRun {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowChildClient) External(ref GetHandleOpts) CustomerBillingExternalRun {
+	return &customerBillingExternalRun{
+		workflowID: ref.WorkflowID,
+		runID:      ref.RunID,
+	}
 }
 
-type customerBillingExternalRun struct{}
-
-func (c customerBillingExternalRun) ID() string {
-	//TODO implement me
-	panic("implement me")
+// Implementing CustomerBillingExternalRun
+type customerBillingExternalRun struct {
+	workflowID string
+	runID      string
 }
 
-func (c customerBillingExternalRun) RunID() string {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingExternalRun) ID() string {
+	return c.workflowID
 }
 
-func (c customerBillingExternalRun) RequestCancellation(ctx workflow.Context) error {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingExternalRun) RunID() string {
+	return c.runID
 }
 
-func (c customerBillingExternalRun) CancelBilling(ctx workflow.Context, req CancelBillingSignal) error {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingExternalRun) RequestCancellation(ctx workflow.Context) error {
+	return workflow.RequestCancelExternalWorkflow(ctx, c.workflowID, c.runID).Get(ctx, nil)
 }
 
-func (c customerBillingExternalRun) CancelBillingAsync(ctx workflow.Context, req CancelBillingSignal) workflow.Future {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingExternalRun) CancelBilling(ctx workflow.Context, req CancelBillingSignal) error {
+	return c.CancelBillingAsync(ctx, req).Get(ctx, nil)
 }
 
-func (c customerBillingExternalRun) SetDiscount(ctx workflow.Context, req SetDiscountSignal) error {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingExternalRun) CancelBillingAsync(ctx workflow.Context, req CancelBillingSignal) workflow.Future {
+	return workflow.SignalExternalWorkflow(ctx, c.workflowID, c.runID, barv1CustomerBillingWorkflowCancelBillingName, req)
 }
 
-func (c customerBillingExternalRun) SetDiscountAsync(ctx workflow.Context, req SetDiscountSignal) workflow.Future {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingExternalRun) SetDiscount(ctx workflow.Context, req SetDiscountSignal) error {
+	return c.SetDiscountAsync(ctx, req).Get(ctx, nil)
 }
 
-type customerBillingWorkflowRun struct{}
-
-func (c customerBillingWorkflowRun) ID() string {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingExternalRun) SetDiscountAsync(ctx workflow.Context, req SetDiscountSignal) workflow.Future {
+	return workflow.SignalExternalWorkflow(ctx, c.workflowID, c.runID, barv1CustomerBillingWorkflowSetDiscountName, req)
 }
 
-func (c customerBillingWorkflowRun) RunID() string {
-	//TODO implement me
-	panic("implement me")
+// Implementing CustomerBillingWorkflowRun
+type customerBillingWorkflowRun struct {
+	client      client.Client
+	workflowRun client.WorkflowRun
 }
 
-func (c customerBillingWorkflowRun) Get(ctx context.Context) (CustomerBillingResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowRun) ID() string {
+	return c.workflowRun.GetID()
 }
 
-func (c customerBillingWorkflowRun) AttemptPayment(ctx context.Context, req AttemptPaymentRequest, mods ...temporal.WorkflowOptionFunc) (res AttemptPaymentResponse, err error) {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowRun) RunID() string {
+	return c.workflowRun.GetRunID()
 }
 
-func (c customerBillingWorkflowRun) GetAccountDetails(ctx context.Context, req GetAccountDetailsRequest, mods ...temporal.WorkflowOptionFunc) (res GetAccountDetailsResult, err error) {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowRun) Get(ctx context.Context) (CustomerBillingResponse, error) {
+	var result CustomerBillingResponse
+	err := c.workflowRun.Get(ctx, &result)
+	return result, err
 }
 
-func (c customerBillingWorkflowRun) CancelBilling(ctx context.Context, req CancelBillingSignal, mods ...temporal.WorkflowOptionFunc) error {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowRun) AttemptPayment(ctx context.Context, req AttemptPaymentRequest, mods ...WorkflowOptionFunc) (AttemptPaymentResponse, error) {
+	options := temporal.NewUpdateOptionsBuilder().
+		WithProvidersWhenSupported(req).
+		WithWorkflowID(c.ID()).
+		WithRunID(c.RunID()).
+		Build()
+
+	updateHandle, err := c.client.UpdateWorkflow(ctx, options)
+	if err != nil {
+		return AttemptPaymentResponse{}, err
+	}
+
+	var result AttemptPaymentResponse
+	err = updateHandle.Get(ctx, &result)
+	return result, err
 }
 
-func (c customerBillingWorkflowRun) SetDiscount(ctx context.Context, req SetDiscountSignal, mods ...temporal.WorkflowOptionFunc) error {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowRun) GetAccountDetails(ctx context.Context, req GetAccountDetailsRequest, mods ...WorkflowOptionFunc) (GetAccountDetailsResult, error) {
+	queryResponse, err := c.client.QueryWorkflow(ctx, c.ID(), c.RunID(), barv1CustomerBillingWorkflowGetAccountDetailsName, req)
+	if err != nil {
+		return GetAccountDetailsResult{}, err
+	}
+
+	var result GetAccountDetailsResult
+	err = queryResponse.Get(&result)
+	return result, err
 }
 
-type customerBillingWorkflowChildRun struct{}
-
-func (c customerBillingWorkflowChildRun) ID() string {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowRun) CancelBilling(ctx context.Context, req CancelBillingSignal, mods ...WorkflowOptionFunc) error {
+	return c.client.SignalWorkflow(ctx, c.ID(), c.RunID(), barv1CustomerBillingWorkflowCancelBillingName, req)
 }
 
-func (c customerBillingWorkflowChildRun) RunID() string {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowRun) SetDiscount(ctx context.Context, req SetDiscountSignal, mods ...WorkflowOptionFunc) error {
+	return c.client.SignalWorkflow(ctx, c.ID(), c.RunID(), barv1CustomerBillingWorkflowSetDiscountName, req)
 }
 
-func (c customerBillingWorkflowChildRun) IsReady() bool {
-	//TODO implement me
-	panic("implement me")
+// Implementing CustomerBillingWorkflowChildRun
+type customerBillingWorkflowChildRun struct {
+	workflowId  string
+	runId       string
+	childFuture workflow.ChildWorkflowFuture
 }
 
-func (c customerBillingWorkflowChildRun) Underlying() workflow.ChildWorkflowFuture {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowChildRun) ID() string {
+	return c.workflowId
 }
 
-func (c customerBillingWorkflowChildRun) Get(ctx workflow.Context) (CustomerBillingResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowChildRun) IsReady() bool {
+	return c.childFuture.IsReady()
 }
 
-func (c customerBillingWorkflowChildRun) CancelBilling(ctx workflow.Context, req CancelBillingSignal) error {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowChildRun) Underlying() workflow.ChildWorkflowFuture {
+	return c.childFuture
 }
 
-func (c customerBillingWorkflowChildRun) SetDiscount(ctx workflow.Context, req SetDiscountSignal) error {
-	//TODO implement me
-	panic("implement me")
+func (c *customerBillingWorkflowChildRun) Get(ctx workflow.Context) (CustomerBillingResponse, error) {
+	var result CustomerBillingResponse
+	err := c.childFuture.Get(ctx, &result)
+	return result, err
+}
+
+func (c *customerBillingWorkflowChildRun) CancelBilling(ctx workflow.Context, req CancelBillingSignal) error {
+	return c.childFuture.SignalChildWorkflow(ctx, barv1CustomerBillingWorkflowCancelBillingName, req).Get(ctx, nil)
+}
+
+func (c *customerBillingWorkflowChildRun) SetDiscount(ctx workflow.Context, req SetDiscountSignal) error {
+	return c.childFuture.SignalChildWorkflow(ctx, barv1CustomerBillingWorkflowSetDiscountName, req).Get(ctx, nil)
 }
